bfc90d3a59c2441efd675bf69195f99cb2c1869d
Charles Meeks <MeeksFamily06@Outlook.com>
2026-04-29 17:46:00 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -98,7 +98,6 @@ echo ""
 
 cd "$MOLLIE_DIR"
 docker compose up -d
-docker cp .htpasswd mollies-api:/etc/nginx/.htpasswd
 info "Containers started. Waiting 20 seconds for MySQL to initialize..."
 sleep 20
 
@@ -139,6 +138,7 @@ else
         warn "Skipping database restore. Site will work but show no locations."
     fi
 fi
+docker cp .htpasswd mollies-api:/etc/nginx/.htpasswd
 
 echo ""
 echo "--- Step 5: Verify ---"
